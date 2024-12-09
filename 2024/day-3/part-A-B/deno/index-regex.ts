2f6f299199888bf4df2bb8131e0ed9eceb45e64f
William Salvidge <william.salvidge@roh.org.uk>
2024-12-09 14:51:37 +0000

    part a but simpler

---
+++ 2024/day-3/part-A-B/deno/index-regex.ts
@@ -0,0 +1,24 @@
+import fs from 'node:fs/promises';
+
+const prepare = async (): Promise<string> => {
+  const file: string = await fs.readFile('../input.txt', 'utf8');
+  return file;
+}
+
+const h = async () => {
+  const out = await prepare();
+
+  const t = /mul\(\d{1,3},\d{1,3}\)/g
+
+  const f = out.match(t);
+  
+  let res = 0;
+  f?.forEach(match => {
+    const tmp = match.slice(4,-1).split(',');
+    res = res + (Number(tmp[0]) * Number(tmp[1]))
+  })
+
+  return res;
+}
+
+console.log(await h());
