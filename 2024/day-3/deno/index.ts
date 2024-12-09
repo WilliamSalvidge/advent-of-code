8cc50006a38735a8a6d74fc2264d9abae373984d
William Salvidge <william.salvidge@roh.org.uk>
2024-12-09 13:45:08 +0000

    day-3 deno

---
+++ 2024/day-3/deno/index.ts
@@ -0,0 +1,64 @@
+import fs from 'node:fs/promises';
+import { multiplyChecker } from './multiplyChecker.ts';
+
+const prepare = async (): Promise<string> => {
+  const file: string = await fs.readFile('../input.txt', 'utf8');
+  return file;
+}
+
+const mainA = async (input: string): Promise<number> => {
+  const out = input;
+
+  let tally = 0;
+  for (let i = 0; i < out.length; i++) {
+    if (out[i] === 'm') {
+        // console.log('------')
+        // console.log(out.slice(i, i + 12))
+        const output = multiplyChecker(out, i, 0, null, null, null, null);
+        // console.log('res:', output);
+        // console.log('current tally:', tally + output);
+        tally = tally + output;
+      }
+    }
+
+  return tally;
+}
+
+const mainB = async (input: string): Promise<number> => {
+  const out = input;
+
+  let tally = 0;
+  let enabled = true;
+  for (let i = 0; i < out.length; i++) {
+    if (out[i] === 'm' && enabled) {
+        // console.log('------')
+        // console.log(out.slice(i, i + 12))
+        const output = multiplyChecker(out, i, 0, null, null, null, null);
+        // console.log('res:', output);
+        // console.log('current tally:', tally + output);
+        tally = tally + output;
+      }
+      
+      if (out[i] === 'd') {
+        if (enabled && out.slice(i, i + 7) === "don't()") {
+          enabled = false;
+          continue;
+        }
+        if (!enabled && out.slice(i, i + 4) === "do()" && out.slice(i, i + 7) !== "don't()") {
+          enabled = true;
+        }
+      }
+
+    }
+  return tally;
+}
+
+const main = async () => {
+  const out = await prepare();
+  const res_a = await mainA(out);
+  const res_b = await mainB(out);
+  return { A: res_a, B: res_b }
+}
+
+const { A, B } = await main()
+console.log('A:', A, 'B:', B);
---
+++ 2024/day-3/deno/multiplyChecker.ts
@@ -0,0 +1,87 @@
+const isNum = (input: string): boolean => {
+  if (input === '0') return true;
+  if (Number(input)) return true;
+  return false
+}
+
+export const multiplyChecker = (line: string, index: number, innerIndex: number, left: number | null, right: number | null, leftLen: number | null, rightLen: number | null): number => {
+  if (innerIndex === 0 && line.slice(index + 0, index + 4) === "mul(") {
+    // console.log('yep');
+    return multiplyChecker(line, index, 4, null, null, null, null)
+  }
+  if (innerIndex === 4) {
+    if (isNum(line[index + innerIndex]) && isNum(line[index + innerIndex + 1]) && isNum(line[index + innerIndex + 2])) {
+     return multiplyChecker(line, index, 7, Number(line[index + innerIndex] + line[index + innerIndex + 1] + line[index + innerIndex + 2]), null, 3, null) 
+    }
+    if (isNum(line[index + innerIndex]) && isNum(line[index + innerIndex + 1])) {
+     return multiplyChecker(line, index, 6, Number(line[index + innerIndex] + line[index + innerIndex + 1]), null, 2, null) 
+    }
+    if (isNum(line[index + innerIndex])) {
+     return multiplyChecker(line, index, 5, Number(line[index + innerIndex]), null, 1, null) 
+    }
+    return 0;
+  }
+  if (innerIndex === 5) {
+    // console.log('5: ',line[index + innerIndex], line[index + innerIndex + 1], line[index + innerIndex + 2], line[index + innerIndex + 3])
+    if (line[index + innerIndex] !== ',') return 0;
+    if (isNum(line[index + innerIndex + 1]) && isNum(line[index + innerIndex + 2]) && isNum(line[index + innerIndex + 3])) {
+     return multiplyChecker(line, index, 9, left, Number(line[index + innerIndex + 1] + line[index + innerIndex + 2] + line[index + innerIndex + 3]), leftLen, 3) 
+    }
+    if (isNum(line[index + innerIndex + 1]) && isNum(line[index + innerIndex + 2])) {
+     return multiplyChecker(line, index, 8, left, Number(line[index + innerIndex + 1] + line[index + innerIndex + 2]), leftLen, 2) 
+    }
+    if (isNum(line[index + innerIndex + 1])) {
+     return multiplyChecker(line, index, 7, left, Number(line[index + innerIndex + 1]), leftLen, 1) 
+    }
+    return 0;
+  }
+
+  if (innerIndex === 6) {
+    // console.log('6: ',line[index + innerIndex], line[index + innerIndex + 1], line[index + innerIndex + 2], line[index + innerIndex + 3])
+    // console.log('6: left: ', left, leftLen, 'right: ', right, rightLen)
+    if (line[index + innerIndex] !== ',') {
+      // console.log('hiya')
+      return 0
+    };
+    if (isNum(line[index + innerIndex + 1]) && isNum(line[index + innerIndex + 2]) && isNum(line[index + innerIndex + 3])) {
+      return multiplyChecker(line, index, 10, left, Number(line[index + innerIndex + 1] + line[index + innerIndex + 2] + line[index + innerIndex + 3]), leftLen, 3) 
+    }
+    if (isNum(line[index + innerIndex + 1]) && isNum(line[index + innerIndex + 2])) {
+     return multiplyChecker(line, index, 9, left, Number(line[index + innerIndex + 1] + line[index + innerIndex + 2]), leftLen, 2) 
+    }
+    if (isNum(line[index + innerIndex + 1])) {
+     return multiplyChecker(line, index, 8, left, Number(line[index + innerIndex + 1]), leftLen, 1) 
+    }
+    return 0;
+  }
+
+  if (innerIndex === 7) {
+    // console.log('7: ',line[index + innerIndex], line[index + innerIndex + 1], line[index + innerIndex + 2], line[index + innerIndex + 3])
+    // console.log('7: left: ', left, leftLen, 'right: ', right, rightLen)
+    if (leftLen === 1 && rightLen === 1 && left && right) {
+      if (line[index + innerIndex] === ')') return left * right;
+      return 0;
+    }
+    if (line[index + innerIndex] !== ',') return 0;
+    if (isNum(line[index + innerIndex + 1]) && isNum(line[index + innerIndex + 2]) && isNum(line[index + innerIndex + 3])) {
+     return multiplyChecker(line, index, 11, left, Number(line[index + innerIndex + 1] + line[index + innerIndex + 2] + line[index + innerIndex + 3]), leftLen, 3) 
+    }
+    if (isNum(line[index + innerIndex + 1]) && isNum(line[index + innerIndex + 2])) {
+     return multiplyChecker(line, index, 10, left, Number(line[index + innerIndex + 1] + line[index + innerIndex + 2]), leftLen, 2) 
+    }
+    if (isNum(line[index + innerIndex + 1])) {
+     return multiplyChecker(line, index, 9, left, Number(line[index + innerIndex + 1]), leftLen, 1) 
+    }
+    return 0;
+  }
+
+  if (innerIndex >= 8) {
+      // console.log('8: ',line[index + innerIndex], line[index + innerIndex + 1], line[index + innerIndex + 2], line[index + innerIndex + 3])
+      // console.log('8: left: ', left, leftLen, 'right: ', right, rightLen)
+      if (line[index + innerIndex] === ')' && left && right) return left * right;
+  }
+
+  // console.log('uh-oh')
+  return 0;
+}
+
